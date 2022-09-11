b5db38ce154c04858de136fc2597f20d4ea2c3b1
Dina <dbcastellanos@gmail.com>
2022-09-11 11:38:17 -0400

    chapter3 end

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -7,4 +8,3 @@ async function bootstrap() {
 }
 bootstrap();
 
-// Chapter 3
--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,12 @@
 /* eslint-disable prettier/prettier */
+import { IsNotEmpty } from "class-validator";
+
+/* eslint-disable prettier/prettier */
 export class CreateTaskDto {
+
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
